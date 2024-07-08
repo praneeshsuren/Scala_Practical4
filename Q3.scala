6eddd4595bfa87733b76d96b656eeeddfa6d8769
praneeshsuren <praneeshsuren@gmail.com>
2024-07-09 00:47:11 +0530

    fix: deleted unnecessary code

--- Q3.scala
+++ Q3.scala
@@ -6,23 +6,19 @@ def toLower(s: String): String = {
     return s.toLowerCase()
 }
 
-def formatNames(name: String)(formatFunc: String => String): String = {
-    formatFunc(name)
-}
-
 def formatNames(x: String, y: Int): String = {
   y match {
     case 1 => toUpper(x) 
     case 2 => toUpper(x.substring(0, 2)) + x.substring(2) 
     case 3 => toLower(x) 
-    case 4 => toUpper(x.head.toString) + x.substring(1, x.length - 1) + toUpper(x.last.toString) 
+    case 4 => toUpper(x.head.toString()) + x.substring(1, x.length - 1) + toUpper(x.last.toString()) 
     case _ => x 
   }
 }
 
-@main def main(): Unit = {
+@main def main3(): Unit = {
     // Test inputs
-    val names = List("Benny", "Niroshan", "Saman", "Kumara")
+    val names = Array("Benny", "Niroshan", "Saman", "Kumara")
 
     // Test outputs
     println(formatNames(names(0), 1));
